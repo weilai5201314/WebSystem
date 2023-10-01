89b9f836096f5672b39dc9ee07ee113a8367f7e9
weilai5201314 <w200111040833@163.com>
2023-10-01 21:32:07 +0800

    (style):add api.cs

---
+++ api/Controllers/api.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class api : Controller
+{
+    // GET
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello,Api!");
+    }
+}
