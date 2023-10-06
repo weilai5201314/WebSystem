639fe56c5879675ce6cb0be80d2f719156f8c523
weilai5201314 <w200111040833@163.com>
2023-10-07 01:27:09 +0800

    (feat):Build some func of UpdateIdentity

---
+++ api/Controllers/Admin/UpdateIdentity.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using server.Mysql.Data; // 导入数据注解命名空间
+
+// 导入数据注解命名空间
+
+namespace server.Controllers.Admin;
+
+public partial class Api
+{
+    [HttpPut("UpdateIdentity")]
+    public IActionResult UpdateIdentity([FromBody] UpdateIdentityRequest request)
+    {
+        //避免参数过多
+        if (request.AddUserGroupIds.Count == 2 && request.RemoveUserGroupIds.Count == 2)
+            return BadRequest("参数过多");
+
+        // 判断增加还是删除
+        if (request.AddUserGroupIds.Count == 2)
+        {
+            int userID = request.AddUserGroupIds[0];
+            int groupID = request.AddUserGroupIds[1];
+
+            // 执行相应的操作，使用 firstUserGroupId 和 secondUserGroupId 进行处理
+            return Ok("add function");
+        }
+
+        if (request.RemoveUserGroupIds.Count == 2)
+        {
+            int userID = request.RemoveUserGroupIds[0];
+            int groupID = request.RemoveUserGroupIds[1];
+            // 处理请求参数不足两个数的情况
+            return Ok("bad function");
+        }
+        else
+            return BadRequest("参数至少应包含两个数。");
+    }
+
+    // 验证管理员账号是否有效
+    private bool IsValidAdmin(string adminAccount)
+    {
+        // 实现验证管理员账号的逻辑，例如从数据库中验证
+        // 如果有效，返回 true，否则返回 false
+        return true;
+    }
+
+    // 验证用户账号是否有效
+    private bool IsValidUser(string userAccount)
+    {
+        // 实现验证用户账号的逻辑，例如从数据库中验证
+        // 如果有效，返回 true，否则返回 false
+        return true;
+    }
+
+    // 添加身份
+    private bool AddIdentityToUser(string userAccount)
+    {
+        // 实现添加身份的逻辑，例如向数据库中添加身份信息
+        // 如果成功，返回 true，否则返回 false
+        return true;
+    }
+
+    // 删除身份
+    private bool RemoveIdentityFromUser(string userAccount)
+    {
+        // 实现删除身份的逻辑，例如从数据库中删除身份信息
+        // 如果成功，返回 true，否则返回 false
+        return true;
+    }
+
+    public class UpdateIdentityRequest
+    {
+        public int UserId { get; set; } // 要操作的用户的ID
+        public List<int> AddUserGroupIds { get; set; } // 要添加的用户组ID列表
+        public List<int> RemoveUserGroupIds { get; set; } // 要删除的用户组ID列表
+    }
+}
