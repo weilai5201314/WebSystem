09df2fa72988294fdf97f529cb21813ce0d85251
weilai5201314 <w200111040833@163.com>
2023-11-25 16:00:24 +0800

    (feat)api:finish SignUp2.cs.

--- api/Controllers/User/SignUp.cs
+++ api/Controllers/User/SignUp.cs
@@ -1,111 +1,7 @@
-﻿// using Microsoft.AspNetCore.Mvc;
-// using server.HashEncry;
-// using server.Mysql.Data;
-// using System.ComponentModel.DataAnnotations; // 导入数据注解命名空间
-// using server.Mysql.Models;
-// using server.Time; // 导入Log实体类所在的命名空间
-//
-// namespace server.Controllers.User
-// {
-//     public partial class Api
-//     {
-//         // 使用属性注入
-//         [FromServices] public MysqlDbContext SignUp_context { get; set; }
-//
-//
-//         [HttpPost("SignUp")]
-//         public IActionResult SignUp([FromBody] SignUpRequest request)
-//         {
-//             // 使用数据注解进行输入验证
-//             // 检测非法输入
-//             var validationContext = new ValidationContext(request, serviceProvider: null, items: null);
-//             var validationResults = new List<ValidationResult>();
-//             if (!Validator.TryValidateObject(request, validationContext, validationResults,
-//                     validateAllProperties: true))
-//             {
-//                 // 如果验证失败，返回错误消息
-//                 LogSignUp(request.Account, false, $"{request.Account} {request.Password}", "非法输入。");
-//                 return BadRequest(validationResults.Select(r => r.ErrorMessage));
-//             }
-//
-//             // 判断是否已经注册
-//             if (SignUp_context.User.Any(u => u.Account == request.Account))
-//             {
-//                 // 记录注册失败的日志
-//                 LogSignUp(request.Account, false, $"{request.Account} {request.Password}", "Username already exists.");
-//                 return BadRequest("Username already exists.");
-//             }
-//
-//             // 使用 PasswordHelper 类的 GenerateSaltAndHash 方法生成盐值和哈希密码
-//             var (salt, hash) = PasswordHelper.GenerateSaltAndHash(request.Password);
-//
-//             // 创建一个新的用户实体
-//             var newUser = new server.Mysql.Models.User
-//             {
-//                 Account = request.Account,
-//                 Pass = hash,
-//                 Salt = salt,
-//                 Status = 0,
-//                 RevertPass = new byte[256]
-//             };
-//
-//             // 将新用户添加到数据库
-//             SignUp_context.User.Add(newUser);
-//             SignUp_context.SaveChanges();
-//
-//             // 记录注册成功的日志
-//             LogSignUp(request.Account, true, $"{request.Account} {request.Password}", "注册成功，等待管理审核。");
-//
-//             return Ok("注册成功，等待管理审核。");
-//         }
-//
-//         // 添加记录注册操作的日志函数
-//         private void LogSignUp(string userName, bool success, string input, string message)
-//         {
-//             DateTime cstTime = TimeHelper.BeijingTime;
-//             // 创建日志实体
-//             var log = new Log
-//             {
-//                 Timestamp = cstTime,
-//                 User = userName,
-//                 Action = "SignUp",
-//                 InputResult = success,
-//                 InputValue = $"UserName: {userName}, Input:{input}",
-//                 ReturnResult = success,
-//                 ReturnValue = message // 这里可以记录注册成功的消息或 Token
-//             };
-//
-//             // 将日志实体添加到数据库中
-//             SignUp_context.Log.Add(log);
-//             SignUp_context.SaveChanges();
-//         }
-//     }
-//
-//     // 注册请求参数格式
-//     public class SignUpRequest
-//     {
-//         [Required(ErrorMessage = "Account is required.")]
-//         [RegularExpression("^[a-zA-Z0-9]{1,20}$", ErrorMessage = "账号只能是20位以内的数字或者字母.")]
-//         public string Account { get; set; }
-//
-//         // 添加输入检测限制
-//         [Required(ErrorMessage = "Password is required.")]
-//         [RegularExpression("^[a-zA-Z0-9!@#$%^&*()_+\\-=[\\]{};:'\\\",<.>/?]+$",
-//             ErrorMessage =
-//                 "Password must be alphanumeric and may include special characters. It should be less than or equal to 30 characters.")]
-//         public string Password { get; set; }
-//     }
-// }
-
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using server.HashEncry;
 using server.Mysql.Data;
-using System;
-using System.Linq;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using server.Mysql.Models;
-using server.Time;
 
 namespace server.Controllers
 {
@@ -143,6 +39,8 @@ namespace server.Controllers
             var newUser = new server.Mysql.Models.User
             {
                 Account = request.Account,
+                Pass = new byte[256],
+                Salt = new byte[256],
                 Status = 0,
                 RevertPass = new byte[256],
                 N = n,
@@ -163,47 +61,6 @@ namespace server.Controllers
 
             return Ok(response);
         }
-
-        //     [HttpPost("SignUp2")]
-        //     public IActionResult SignUp2([FromBody] SignUp2Request request)
-        //     {
-        //         var userRegistration = SignUp_context.User.Find(request.Id);
-        //
-        //         if (userRegistration == null)
-        //         {
-        //             return BadRequest("Invalid registration ID.");
-        //         }
-        //
-        //         var calculatedHash =
-        //             PasswordHelper.HashWithIterations(request.Password, userRegistration.R, userRegistration.N);
-        //
-        //         if (calculatedHash.SequenceEqual(request.Hash))
-        //         {
-        //             var salt = PasswordHelper.GenerateSalt();
-        //             var hash = PasswordHelper.HashWithIterations(request.Password, salt, userRegistration.N + 1);
-        //
-        //             var newUser = new server.Mysql.Models.User
-        //             {
-        //                 Account = userRegistration.Account,
-        //                 Pass = hash,
-        //                 Salt = salt,
-        //                 Status = 0,
-        //                 RevertPass = new byte[256],
-        //                 N = userRegistration.N,
-        //                 R = userRegistration.R
-        //             };
-        //
-        //             SignUp_context.User.Add(newUser);
-        //             SignUp_context.SaveChanges();
-        //
-        //             TypeLog(newUser.Account, "Signup", true, $"{request.id} ", false, "注册成功，等待管理审核。");
-        //             return Ok("注册成功，等待管理审核。");
-        //         }
-        //         else
-        //         {
-        //             return BadRequest("Invalid password hash.");
-        //         }
-        //     }
     }
 
     public class SignUpRequest
@@ -211,13 +68,5 @@ namespace server.Controllers
         [Required(ErrorMessage = "Account is required.")]
         [RegularExpression("^[a-zA-Z0-9]{1,20}$", ErrorMessage = "账号只能是20位以内的数字或者字母.")]
         public string Account { get; set; }
-    }
-
-    public class SignUp2Request
-    {
-        public Guid Id { get; set; }
-        public int id { get; set; }
-        public byte[] Hash { get; set; }
-        public string Password { get; set; }
     }
 }
--- api/Controllers/User/SignUp2.cs
+++ api/Controllers/User/SignUp2.cs
@@ -1,1 +1,53 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace server.Controllers
+{
+    public partial class Api
+    {
+        // 使用属性注入
+
+
+        [HttpPost("user/SignUp2")]
+        public IActionResult SignUp2([FromBody] SignUp2Request request)
+        {
+            // 判断用户是否已存在
+            if (!(SignUp_context.User.Any(u => u.Account == request.Account)))
+            {
+                return BadRequest("User not found.");
+            }
+
+            var user = DbContext.User.FirstOrDefault(u => u.Account == request.Account);
+            //  提取 N+1 次迭代值入库
+            //  先转化
+            byte[] passwordBytes;
+            try
+            {
+                user.Pass = Convert.FromBase64String(request.Password);
+            }
+            catch (FormatException)
+            {
+                return BadRequest("Invalid Base64 string for password.");
+            }
+
+
+            //  开始保存
+            SignUp_context.User.Update(user);
+            SignUp_context.SaveChanges();
+
+            TypeLog(request.Account, "Signup", true, $"{request.Account} ", false, "注册成功，等待管理审核。");
+            return Ok("注册成功，等待管理审核。");
+        }
+    }
+
+    public class SignUp2Request
+    {
+        [Required(ErrorMessage = "Account is required.")]
+        [RegularExpression("^[a-zA-Z0-9]{1,20}$", ErrorMessage = "账号只能是20位以内的数字或者字母.")]
+        public string Account { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [RegularExpression("^[a-zA-Z0-9]{1,20}$", ErrorMessage = "密码只能是20位以内的数字或者字母.")]
+        public string Password { get; set; }
+    }
+}
