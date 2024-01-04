6427523a13c5e8adb9326fe5463c739e28da64d5
weilai5201314 <w200111040833@163.com>
2024-01-04 18:03:44 +0800

    (Build)client:Build fileControl to show file system.

--- client/src/MainWindow.xaml.cs
+++ client/src/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 using client.admin;
+using client.file;
+// using client.File;
 using client.user;
 
 
@@ -19,11 +21,11 @@ namespace client
         private void ToShowInfo(object sender, RoutedEventArgs e)
         {
             ShowInfo newWindow = new ShowInfo();
-            this.IsEnabled = false; //禁用原来的窗口
+            IsEnabled = false; //禁用原来的窗口
             // 订阅新窗口的 Closed 事件，在窗口关闭时恢复原始窗口的可用状态
             newWindow.Closed += (sender, e) =>
             {
-                this.IsEnabled = true;
+                IsEnabled = true;
                 //  当修改页面关闭后，才 清空输入
                 //     Account.Text = "";
                 //     Tip.Text = "";
@@ -35,7 +37,7 @@ namespace client
         private void ToShowDaily(object sender, RoutedEventArgs e)
         {
             ShowDaily newWindow = new ShowDaily();
-            this.IsEnabled = false; //禁用原来的窗口
+            IsEnabled = false; //禁用原来的窗口
             // 订阅新窗口的 Closed 事件，在窗口关闭时恢复原始窗口的可用状态
             newWindow.Closed += (sender, e) =>
             {
@@ -51,7 +53,7 @@ namespace client
         private void ToRevertPass(object sender, RoutedEventArgs e)
         {
             RevertPass newWindow = new RevertPass();
-            this.IsEnabled = false; //禁用原来的窗口
+            IsEnabled = false; //禁用原来的窗口
             // 订阅新窗口的 Closed 事件，在窗口关闭时恢复原始窗口的可用状态
             newWindow.Closed += (sender, e) =>
             {
@@ -69,6 +71,17 @@ namespace client
             LogIn newWindow = new LogIn();
             newWindow.Show();
             GetWindow(this)?.Close();
+        }
+
+        private void JumpFileControl(object sender, RoutedEventArgs e)
+        {
+            fileControl newWindow = new fileControl();
+            IsEnabled = false;
+            newWindow.Closed += (sender, e) =>
+            {
+                IsEnabled = true;
+            };
+            newWindow.Show();
         }
     }
 }
---
+++ client/src/file/fileControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace client.file;
+
+public partial class fileControl : Window
+{
+    public fileControl()
+    {
+        InitializeComponent();
+    }
+    
+    private async void ReadFile_Click(object sender, RoutedEventArgs e)
+    {
+        // 调用后端API，读取文件
+        // 更新界面或展示文件详情
+    }
+
+    private async void DeleteFile_Click(object sender, RoutedEventArgs e)
+    {
+        // 调用后端API，删除文件
+        // 更新界面或显示删除成功信息
+    }
+
+}
