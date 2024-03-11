fc211d149a4ceb26a5ec61468d0b299751087f61
weilai5201314 <w200111040833@163.com>
2024-03-11 20:16:21 +0800

    chore(projects): 总结部分文件

--- client/obj/Debug/net7.0-windows/client_vf5mkcrh_wpftmp.AssemblyInfo.cs
+++ client/obj/Debug/net7.0-windows/client_vf5mkcrh_wpftmp.AssemblyInfo.cs
@@ -10,13 +10,15 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("api")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+623454b92c66b32839965e64d8789b6e4ef3f519")]
-[assembly: System.Reflection.AssemblyProductAttribute("api")]
-[assembly: System.Reflection.AssemblyTitleAttribute("api")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af07f19925f859e3b79a304620a614d89ba947c8")]
+[assembly: System.Reflection.AssemblyProductAttribute("client")]
+[assembly: System.Reflection.AssemblyTitleAttribute("client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
 
--- test/obj/Debug/net7.0/test.AssemblyInfo.cs
+++ test/obj/Debug/net7.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af07f19925f859e3b79a304620a614d89ba947c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+623454b92c66b32839965e64d8789b6e4ef3f519")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
